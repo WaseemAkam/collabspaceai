fd36dc329eeec1732a760f8566917d14accc3caf
WaseemAkram <wasimakram03004@gmail.com>
2026-04-23 23:27:54 +0530

    frontend deploy fix

--- server/index.js
+++ server/index.js
@@ -12,12 +12,19 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'] }
+  cors: { 
+    origin: true, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }
 });
 
 app.set('io', io);
 
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
